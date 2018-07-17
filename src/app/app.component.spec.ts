fc19cf3c4a9756c3a417a923a3c62a36ff11068a
Jacopo Mattei <j.mattei@quidinfo.it>
2018-07-17 14:39:29 +0200

    Inserito generatore citazioni e relativi test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -27,6 +27,32 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome');
   }));
+
+  it(`should have valid quote`, async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    app.setDailyQuote();
+    expect(app.dailyQuote).toBeDefined();
+  }));
+
+  it('should render refresh button', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    // fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('button').textContent).toContain('Refresh');
+  }));
+
+  it('Refresh should update the quote', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    const compiled = fixture.debugElement.nativeElement;
+    let button = fixture.debugElement.nativeElement.querySelector('button');
+    button.click();
+    const firstQuote = app.dailyQuote;
+    button.click();
+    const secondQuote = app.dailyQuote;
+    expect(firstQuote !== secondQuote).toBeTruthy();
+  });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  dailyQuote: string;
+  quotesArray: string[] = [
+    'Se il mondo fosse chiaro, l’arte non esisterebbe',
+    'Al mondo non c’è coraggio e non c’è paura, ci sono solo coscienza e incoscienza. La coscienza è paura, l’incoscienza è coraggio',
+    'Il segreto della creatività è saper nascondere le proprie fonti',
+    'La letteratura sta alla poesia come la menzogna alla verità',
+    'Le persone non fanno i viaggi, sono i viaggi che fanno le persone.',
+    'Un vincitore è semplicemente un sognatore che non si è mai arreso',
+    'La violenza è un sintomo di impotenza'
+  ];
+
+  ngOnInit() {
+    this.setDailyQuote();
+  }
+
+  setDailyQuote() {
+    this.dailyQuote = this.quotesArray[Math.floor(Math.random() * this.quotesArray.length)];
+  }
 }
